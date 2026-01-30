464d7e16e1c502109420c52eb312b19f6fb06fd4
Divyasree-7 <divyasree.dammalapati4@gmail.com>
2026-01-30 10:03:00 +0530

    Commiting changes

--- Search.java
+++ Search.java
@@ -1,11 +1,1 @@
-public class Search{
-public static void main(String[] args){
-int [] arr={5,6,7,8,9};
-int key=7;
-for(int i=0;i<=arr.length;i++){
-if(arr[i]==key){
-System.out.println("found");}
-else{
-System.out.println("not found");
-}
-}}}
+this is modified file
