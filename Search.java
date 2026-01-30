43436d1a2058fa4eda5595bc3e4f975b3a82422d
Divyasree-7 <divyasree.dammalapati4@gmail.com>
2026-01-30 10:00:31 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,11 @@
+public class Search{
+public static void main(String[] args){
+int [] arr={5,6,7,8,9};
+int key=7;
+for(int i=0;i<=arr.length;i++){
+if(arr[i]==key){
+System.out.println("found");}
+else{
+System.out.println("not found");
+}
+}}}
